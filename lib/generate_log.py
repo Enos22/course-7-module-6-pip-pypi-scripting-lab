517aec8409bff8570342478c440c56ea56268ffa
Emos Arenga <enos.arenga@gmail.com>
2026-09-08 15:34:03 +0300

    Add automation script with API and file output

--- lib/generate_log.py
+++ lib/generate_log.py
@@ -1,16 +1,20 @@
-from datetime import datetime
 import requests
 from datetime import datetime
 import os
 
 def generate_log(data):
+     if not isinstance(data, list):
+         raise ValueError("Input data must be a list.")
+
      log_data = ["User logged in", "User updated profile", "Report exported"]
      filename = f"log_{datetime.now().strftime('%Y%m%d')}.txt"
+
      with open(filename, 'w') as file:
         for entry in log_data:
             file.write(f"{entry}\n")
 
         print(f"Log written to {filename}")
+        return filename
 
 #fetch data using package instaled
 def fetch_data():
